a43b825b66ec57ad18a6b270eec2e9619de61501
Kaivv1 <kvalchevv1@gmail.com>
2023-09-02 02:58:10 +0300

    Added a hamburger button for smaller resolutions

--- README.md
+++ README.md
@@ -7,8 +7,12 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
+## Responsive for all devices
+
 ### Project setup <br>
+
 npm install
 
 ### Project startup <br>
+
 npm run dev
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,57 @@
 import { Link } from 'react-router-dom';
 import Username from '../user/Username';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isDropDown, setIsDropDown] = useState(false);
+  const toggleDropDown = () => {
+    setIsDropDown(!isDropDown);
+  };
+
   return (
     <header className="flex items-center justify-between border-b-2 border-blue bg-darker px-4 py-5 uppercase tracking-wide sm:px-6">
       <div className="text-lg font-semibold text-light md:text-xl xl:text-2xl">
         Dummy Project
       </div>
 
-      <div className="space-x-2 text-sm font-medium text-light md:space-x-3 md:text-base xl:text-lg">
+      <button
+        className="text-2xl text-light md:hidden"
+        onClick={toggleDropDown}
+      >
+        {isDropDown ? (
+          <FontAwesomeIcon icon={faTimes} />
+        ) : (
+          <FontAwesomeIcon icon={faBars} />
+        )}
+      </button>
+
+      {isDropDown && (
+        <div className="absolute right-0 top-16 mt-2 flex flex-col space-y-2 rounded-md border border-blue  bg-darker p-2 text-base font-medium text-lighter shadow-md md:hidden">
+          <Link
+            to="/"
+            className="block hover:underline hover:underline-offset-2"
+          >
+            {' '}
+            Home
+          </Link>
+          <Link
+            to="/products"
+            className="block hover:underline hover:underline-offset-2"
+          >
+            Menu
+          </Link>
+          <Link
+            to={'/createProduct'}
+            className="block hover:underline hover:underline-offset-2"
+          >
+            Create listing
+          </Link>
+        </div>
+      )}
+
+      <div className="hidden space-x-2 text-xs font-medium text-light sm:text-sm md:block md:space-x-3 md:text-base xl:text-lg">
         <Link to="/" className="hover:underline hover:underline-offset-2">
           {' '}
           Home
